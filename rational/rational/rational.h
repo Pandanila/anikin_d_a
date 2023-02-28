9a50bbf941b59d42afdf001ca178254b23975cd4
Pandanila <97802843+Pandanila@users.noreply.github.com>
2023-02-28 22:48:45 +0400

    Add files via upload

---
+++ rational/rational/rational.h
@@ -0,0 +1,50 @@
+//
+// Created by danan on 28.02.2023.
+//
+
+#ifndef RATIONAL_RATIONAL_H
+#define RATIONAL_RATIONAL_H
+
+class Rational{
+private:
+    int32_t num{0};
+    int32_t denum{1};
+    static const char separator = '/';
+public:
+    Rational() = default;
+    Rational(const Rational&) = default;
+    Rational(int32_t in_num, int32_t in_denum);
+    ~Rational() = default;
+    std::istream& ReadFrom(std::istream& istrm);
+    std::ostream& WriteTo(std::ostream& ostrm) const;
+    Rational& gcd();
+    Rational& common_denom(Rational& second);
+    Rational& operator+=(Rational& rhs);
+    Rational& operator-=(Rational& rhs);
+    Rational& operator*=(const Rational& rhs);
+    Rational& operator/=(const Rational& rhs);
+    bool operator==(const Rational& rhs) const;
+    bool operator!=(const Rational &rhs) const;
+    bool operator>( Rational &rhs);
+    bool operator<( Rational &rhs);
+    bool operator>=( Rational &rhs);
+    bool operator<=( Rational &rhs);
+};
+
+//input-output
+inline std::istream& operator>>(std::istream& istrm, Rational& rhs){
+    return rhs.ReadFrom(istrm);
+};
+inline std::ostream& operator<<(std::ostream& ostrm, Rational& rhs){
+    return rhs.WriteTo(ostrm);
+};
+//Algebra
+Rational& operator+(Rational& lhs, Rational& rhs);
+Rational& operator-(Rational& lhs, Rational& rhs);
+Rational& operator*(Rational& lhs, Rational& rhs);
+Rational& operator/(Rational& lhs, Rational& rhs);
+
+
+
+
+#endif //RATIONAL_RATIONAL_H
