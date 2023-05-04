f9858ca63bf427481088c57ce2269deb54ce2a26
Pandanila <97802843+Pandanila@users.noreply.github.com>
2023-05-04 22:59:49 +0400

    Add files via upload

---
+++ anikin_d_a/prj.lab/arrayd/include/arrayd/arrayd.hpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+#pragma once
+#ifndef ARRAYD_ARRAYD_HPP_20230304
+#define ARRAYD_ARRAYD_HPP_20230304
+
+const std::exception out_of_range("Error: Out of range");
+
+class ArrayD {
+public:
+	ArrayD() = default;
+	ArrayD(const ArrayD& arr);
+	explicit ArrayD(const std::ptrdiff_t size);
+	ArrayD& operator=(const ArrayD& rhs);
+	~ArrayD() { delete[] data_; }
+	std::ptrdiff_t Ssize() const noexcept { return size_; };
+	double& operator[](const std::ptrdiff_t i);
+	const double& operator[](const std::ptrdiff_t i) const;
+	void Resize(const std::ptrdiff_t size);
+	void Remove(std::ptrdiff_t index);
+	void Insert(double val, std::ptrdiff_t index);
+	//
+	std::ptrdiff_t ssize() const noexcept { return size_; };
+	void resize(const std::ptrdiff_t size);
+	void remove(std::ptrdiff_t index);
+	void insert(double val, std::ptrdiff_t index);
+private:
+	std::ptrdiff_t capacity_{ 0 };
+	std::ptrdiff_t size_{ 0 };
+	double* data_{ nullptr };
+};
+
+#endif
